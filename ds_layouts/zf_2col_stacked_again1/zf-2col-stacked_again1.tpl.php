2def6445d088ddc7460acf5003dc721355d215a6
svenA <svenangermark@Svens-MacBook-Pro-2.local>
2016-12-08 21:21:09 +0100

    8dec

---
+++ ds_layouts/zf_2col_stacked_again1/zf-2col-stacked_again1.tpl.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * Template for Zurb Foundation Two column stacked Display Suite layout. 
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="zf-2col-stacked <?php print $classes;?>">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php if (!empty($header)): ?>
+    <div class="<?php print $zf_wrapper_classes; ?>">
+      <<?php print $header_wrapper ?> class="group-header<?php print $header_classes; ?>">
+      <?php print $header; ?>
+      </<?php print $header_wrapper ?>>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($left) || !empty($right)): ?>
+    <div class="<?php print $zf_wrapper_classes; ?>">
+  		<?php if ($left): ?>  
+      		<<?php print $left_wrapper ?> class="group-left large-6 small-12 columns<?php print $left_classes; ?>">
+      		<?php print $left; ?>
+      		</<?php print $left_wrapper ?>>
+
+      		<<?php print $right_wrapper ?> class="group-right large-6 small-12 columns<?php print $right_classes; ?>">
+      		<?php print $right; ?>
+      		</<?php print $right_wrapper ?>>
+      	<?php else: ?>
+      		<<?php print $right_wrapper ?> class="group-right small-12 columns<?php print $right_classes; ?>">
+      			<?php print $right; ?>
+      			</<?php print $right_wrapper ?>>
+      	<?php endif; ?>
+    </div>
+  <?php endif; ?>
+  
+  
+
+  <?php if (!empty($footer)): ?>
+    <div class="<?php print $zf_wrapper_classes; ?>">
+      <<?php print $footer_wrapper ?> class="group-footer<?php print $footer_classes; ?>">
+      <?php print $footer; ?>
+      </<?php print $footer_wrapper ?>>
+    </div>
+  <?php endif; ?>
+
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
